2707aabd71eef627a0b2b5be5086c7fad6510462
Yassine Zemmouri <yzemmouri.eng@gmail.com>
2022-03-02 07:17:51 -0500

    find user by id

--- src/main/java/com/coinassets/backend/controller/UsersController.java
+++ src/main/java/com/coinassets/backend/controller/UsersController.java
@@ -3,15 +3,18 @@ package com.coinassets.backend.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.coinassets.backend.exceptions.ResourceNotFoundException;
 import com.coinassets.backend.models.Users;
 import com.coinassets.backend.repositories.UsersRepository;
 
 @RestController
-@RequestMapping("/api/v1/")
+@RequestMapping("/api/v1")
 public class UsersController {
 	@Autowired
 	private UsersRepository userRepo;
@@ -19,5 +22,11 @@ public class UsersController {
 	public List<Users> getAllUsers(){
 		return userRepo.findAll();
 	}
-
+	
+	@GetMapping("/user/{id}")
+	public ResponseEntity<Users> getUserById(@PathVariable int id){
+		Users user = userRepo.findById(id)
+				.orElseThrow(()-> new ResourceNotFoundException("User not found."));
+		return ResponseEntity.ok(user);
+	}
 }
