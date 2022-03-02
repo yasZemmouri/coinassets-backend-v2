e2a2979cf0bb796a59fe669ab8bf1a4826adc425
Yassine Zemmouri <yzemmouri.eng@gmail.com>
2022-03-02 07:25:03 -0500

    post new user

--- src/main/java/com/coinassets/backend/controller/UsersController.java
+++ src/main/java/com/coinassets/backend/controller/UsersController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,5 +30,10 @@ public class UsersController {
 		Users user = userRepo.findById(id)
 				.orElseThrow(()-> new ResourceNotFoundException("User not found."));
 		return ResponseEntity.ok(user);
+	}
+	
+	@PostMapping("createuser")
+	public Users newUser(@RequestBody Users user) {
+		return userRepo.save(user);
 	}
 }
